9707b814007a39185761f0ef75b564f968bde47d
EXPERT23164 <artmhitar2@gmail.com>
2025-05-30 13:09:41 +0300

    Add files via upload

---
+++ практика6/задание2/prakt 6.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+void CheckNumber(int number)
+{
+    if (number % 2 == 0)
+        throw new ArithmeticException("Чётное число");
+    else
+        throw new OverflowException("Нечётное число");
+}
+
+
+while (true)
+{
+    try
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            CheckNumber(num);
+        }
+
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("число нечётное");
+        }
+
+        catch (ArithmeticException ex)
+        {
+            Console.WriteLine("число чётное");
+        }
+      
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Выход из программы");
+        break;
+    }
+}
